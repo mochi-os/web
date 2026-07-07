acea7315251cb0264696bcfb2aa4664dfed79d23
Numan <numan.saleem08@gmail.com>
2026-07-07 12:17:42 +0000

    add Attachment component for rendering file items and export from shadcn library

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
 export * from './components/ui/alert-dialog'
 export * from './components/ui/alert'
 export * from './components/ui/avatar'
+export * from './components/ui/attachment'
 export * from './components/ui/badge'
 export * from './components/ui/button'
 export * from './components/ui/calendar'
